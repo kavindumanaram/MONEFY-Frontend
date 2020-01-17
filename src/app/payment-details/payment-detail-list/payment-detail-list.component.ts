c66f127f739216eca50bda8651f54a8cb60783c4
Kavindu Manaram <kavindu@calcey.com>
2020-01-17 21:08:45 +0530

    add payment detail list function

--- src/app/payment-details/payment-detail-list/payment-detail-list.component.ts
+++ src/app/payment-details/payment-detail-list/payment-detail-list.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { PaymentDetailService } from "src/app/shared/payment-detail.service";
 
 @Component({
-  selector: 'app-payment-detail-list',
-  templateUrl: './payment-detail-list.component.html',
+  selector: "app-payment-detail-list",
+  templateUrl: "./payment-detail-list.component.html",
   styles: []
 })
 export class PaymentDetailListComponent implements OnInit {
-
-  constructor() { }
+  constructor(private service: PaymentDetailService) {}
 
   ngOnInit() {
+    this.service.refreshList();
   }
-
 }
--- src/app/shared/payment-detail.service.ts
+++ src/app/shared/payment-detail.service.ts
@@ -8,10 +8,18 @@ import { HttpClient } from "@angular/common/http";
 export class PaymentDetailService {
   formData: PaymentDetail;
   readonly rootURL = "http://localhost:53546/api/";
+  list: PaymentDetail[];
 
   constructor(private http: HttpClient) {}
 
   postPaymentDetail(FormData: PaymentDetail) {
     return this.http.post(this.rootURL + "paymentdetails", FormData);
+  }
+
+  refreshList() {
+    this.http
+      .get(this.rootURL + "paymentdetails")
+      .toPromise()
+      .then(res => (this.list = res as PaymentDetail[]));
   }
 }
